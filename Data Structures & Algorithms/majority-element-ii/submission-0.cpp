4edf47d5967aee734fb18ce19f59da14f03ed85c
Abdulrahman Nabil <a.nabil.dev@gmail.com>
2026-08-28 20:28:21 +0300

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution 
+{
+public:
+    vector<int> majorityElement(vector<int>& nums) 
+    {
+        vector<int> mostFrequent;
+        unordered_map<int,int> freqMap;
+        for(int i=0; i<nums.size(); i++)
+        {
+            freqMap[nums[i]]++;
+        }
+        for(auto & [number, freq]: freqMap)
+        {
+            if(freq>nums.size()/3)
+                mostFrequent.push_back(number);
+            if(mostFrequent.size()>1)
+                break;
+        }
+        return mostFrequent;
+        
+    }
+};          
